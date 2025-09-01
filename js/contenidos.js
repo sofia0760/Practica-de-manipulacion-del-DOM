8fa1b3d38e87bb872e5729f74270fd3470314c1c
sofia camelo <camelojimenezsofia20@gmail.com>
2025-09-01 15:48:40 -0500

    se agrego una funcionalidad para cambiarle el contenido

---
+++ js/contenidos.js
@@ -0,0 +1,5 @@
+const cajaUnica = document.getElementById ('caja-unica');
+
+cajaUnica.innerText = "cambio en el texto  #1 con innerText";
+cajaUnica.innerHTML = "cambio en el texto #2 con inneHTML";
+cajaUnica.textContent = "cambio en el texto #3 con textContent";
